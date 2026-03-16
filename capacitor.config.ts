3e5c77817a35fa2460b0a20debef4ee0069189b5
plantapprnscpalmarito <platapprnscpalmarito@gmail.com>
2026-03-16 17:41:27 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.plantapp.palmarito',
+  appName: 'Plantapp',
+  webDir: 'out'
+};
+
+export default config;
